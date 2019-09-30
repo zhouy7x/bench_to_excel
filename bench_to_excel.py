aa6e5ab6d63adf9c5cb756fd59fed8929b706268
SunChangmei <sheriscm@163.com>
2019-09-30 15:36:57 +0800

    feat:    enable to extract jetstream by html/txt/webarchive          add the mode : simple/all

--- bench_to_excel.py
+++ bench_to_excel.py
@@ -1,30 +1,35 @@
 #!/usr/bin/python3
 import os
 import re
-# import pandas
+import pandas as pd
 from pandas import DataFrame
 
 import utils
+import argparse
 
-# pandas.
+parser = argparse.ArgumentParser()
+# parser.usage = help_msg
+parser.add_argument('-m', '--mode', type=str, choices=['simple', 'all'], default="simple",
+                    help="return only scores or all outputs of subcases")
+args = parser.parse_args()
+mode = args.mode
 
 class Base(object):
-    pass
+    def __init__(self, name, folder, writer, mode="simple"):
+        self.name = name
+        self.folder = folder
+        self.writer = writer
+        self.df = DataFrame()
+        self.mode = mode
 
 
 class JetStream2(Base):
     def __init__(self, writer):
-        super(JetStream2, self).__init__()
-        self.name = "jetstream2"
-        self.folder = "jetstream2"
-        self.writer = writer
-        self.df = DataFrame()
+        Base.__init__(self, 'jetstream2', 'jetstream2', writer, mode)
 
     def run(self):
         src_ls = os.popen("ls %s" % self.folder).readlines()
         src_ls = list(map(lambda x: x.replace('\n', ''), src_ls))
-        # print(src_ls)
-
         i = 0
         for src in src_ls:
             i += 1
@@ -35,6 +40,12 @@ class JetStream2(Base):
                 self.df['name'] = names
             self.df[i] = scores
 
+        stdev = self.df.std(axis=1)
+        mean = self.df.mean(axis=1)
+        stdev_mean = stdev / mean
+        self.df['stdev'] = stdev
+        self.df['average'] = mean
+        self.df['stdev/average'] = stdev_mean.apply(lambda x: float(x))
         self.df.to_excel(self.writer, index=False, sheet_name=self.name)
 
     def _run(self, file):
@@ -42,24 +53,93 @@ class JetStream2(Base):
         score_ls = []
         with open(file) as f:
             content = f.read()
-        cont = content.replace("=\n", "")
-        score = re.search(r'<div class=3D"score">(\d+\.\d+)</div>', cont).group(1)
-        name_ls.append('Score')
-        score_ls.append(float(score))
-        cont_ls = re.findall(r'<h3[\d\D]*?><[\d\D]*?>(.*?)</a></h3>[\d\D]*?<h4[\d\D]*?>(.*?)</h4>', cont, re.S)
-        for i in cont_ls:
-            name_ls.append(i[0])
-            score_ls.append(float(i[1]))
+
+        if file.endswith('mhtml'):
+            cont = content.replace("=\n", "")
+            score = re.search(r'<div class=3D"score">(\d+\.\d+)</div>', cont).group(1)
+            name_ls.append('Score')
+            score_ls.append(float(score))
+            cont_ls = re.findall(r'<h3[\d\D]*?><[\d\D]*?>(.*?)</a></h3>[\d\D]*?<h4[\d\D]*?>(.*?)</h4>', cont, re.S)
+            for i in cont_ls:
+                name_ls.append(i[0])
+                score_ls.append(float(i[1]))
+        elif file.endswith('txt'):
+            cont_ls = re.findall(r'Running (.*?):(.*?)Score: *([0-9]*\.?[0-9]+)', content, re.S)
+            total_score = re.findall(r"Total Score: *([0-9]*\.?[0-9]+)", content)
+
+            name_ls.append('Score')
+            score_ls.append(float(total_score[0]))
+
+            if self.mode == 'simple':
+                for i in cont_ls:
+                    name_ls.append(i[0])
+                    score_ls.append(float(i[2]))
+            else:
+                name_ls, score_ls = [], []
+                for i in cont_ls:
+                    name_ls.append(i[0])
+                    score_ls.append(float(i[2]))
+                    others = i[1].replace("\n", "")
+                    others = re.findall(r" *(.*?): *([0-9]*\.?[0-9]+)", others, re.S)
+                    for j in others:
+                        name_ls.append("____" + j[0])
+                        score_ls.append(float(j[1]))
+
+        elif file.endswith('htm') or file.endswith('html'):
+            cont_ls = re.findall(r'<div class="benchmark benchmark-done"(.*?)<h3 class="benchmark-name">(.*?)">(.*?)'
+                                 r'</a></h3>(.*?)<h4 class="score"(.*?)">([0-9]*\.?[0-9]+)</h4><p><span class="result"'
+                                 r'>(.*?)</div>', content, re.S)
+
+            name_ls.append('total_socre')
+
+            cont_total = re.search(r'<div id="result-summary" class="done"><div class="score">([0-9]*\.?[0-9]+)</div>'
+                                   r'<label>Score</label></div>', content, re.S).group(1)
+            score_ls.append(float(cont_total))
+
+            if self.mode == 'simple':
+                for i in cont_ls:
+                    name_ls.append(i[2])
+                    score_ls.append(float(i[5]))
+            else:
+                for i in cont_ls:
+
+                    name_ls.append(i[2])
+                    score_ls.append(float(i[5]))
+                    date = re.findall(
+                        r'<span class="result"><span id="(.*?)">([0-9]*\.?[0-9]+)</span><label>(.*?)</label></span>',
+                        i[6], re.S)
+
+                    for j in date:
+                        name_case = '____' + j[2]
+                        name_ls.append(name_case)
+                        score_ls.append(float(j[1]))
+
+        elif file.endswith('webarchive'):
+            name_ls.append('total_socre')
+            cont_total = re.search(r'<div id="result-summary" class="done"><div class="score">([0-9]*\.?[0-9]+)</div>'
+                                   r'<label>Score</label></div>', content, re.S).group(1)
+            score_ls.append(float(cont_total))
+            cont_ls = re.findall(r'<div class="benchmark benchmark-done"(.*?)<h3 class="benchmark-name">(.*?)">(.*?)'
+                                 r'</a></h3>(.*?)<h4 class="score"(.*?)">([0-9]*\.?[0-9]+)</h4><p>(.*?)</p></div>',
+                                 content, re.S)
+            for i in cont_ls:
+                name_ls.append(i[2])
+                score_ls.append(float(i[5]))
+                date = re.findall(
+                    r'<span class="result"><span id="(.*?)">([0-9]*\.?[0-9]+)</span><label>(.*?)</label></span>', i[6],
+                    re.S)
+                for j in date:
+                    name_case = '____' + j[2]
+                    name_ls.append(name_case)
+                    score_ls.append(float(j[1]))
+
         return name_ls, score_ls
 
 
 class Speedometer2(Base):
+
     def __init__(self, writer):
-        super(Speedometer2, self).__init__()
-        self.name = "speedometer2"
-        self.folder = "speedometer2"
-        self.writer = writer
-        self.df = DataFrame()
+        Base.__init__(self, "speedometer2", "speedometer2", writer)
 
     def run(self):
         src_ls = os.popen("ls %s" % self.folder).readlines()
@@ -82,29 +162,41 @@ class Speedometer2(Base):
         score_ls = []
         with open(file) as f:
             content = f.read()
-        cont = content.replace("=\n", "")
-        score = re.search(r'<span id=3D"geomean-score">(\d+\.\d+)</span>', cont).group(1)
-        name_ls.append('Score')
-        score_ls.append(float(score))
-        a = re.findall(r'<table class=3D"results-table">[\d\D]*?</table>', content)
-        for i in a:
-            if "Subcase" in i:
-                i = i.replace("=\n", "")
-                g = re.findall(r'<th>(.*?)</th>[\d\D]*?<td>(.*?)</td>[\d\D]*?<td>.*?</td>', i)
-                item_list = g[1:]
-                for item in item_list:
-                    name_ls.append(item[0])
-                    score_ls.append(float(item[1]))
+        if file.endswith('mhtml'):
+            cont = content.replace("=\n", "")
+            score = re.search(r'<span id=3D"geomean-score">(\d+\.\d+)</span>', cont).group(1)
+            name_ls.append('Score')
+            score_ls.append(float(score))
+            a = re.findall(r'<table class=3D"results-table">[\d\D]*?</table>', content)
+            for i in a:
+                if "Subcase" in i:
+                    i = i.replace("=\n", "")
+                    g = re.findall(r'<th>(.*?)</th>[\d\D]*?<td>(.*?)</td>[\d\D]*?<td>.*?</td>', i)
+                    item_list = g[1:]
+                    for item in item_list:
+                        name_ls.append(item[0])
+                        score_ls.append(float(item[1]))
+        elif file.endswith('html'):
+            a = re.findall(r'<table class="results-table">(.*?)</table>', content)
+            b = a[1]
+            b = b.replace("<tr><th>Subcase</th><td>Score (runs/min)</td><td>Time (ms)</td></tr>", '')
+            cont_ls = re.findall(r'<tr><th>(.*?)</th><td>([0-9]*\.?[0-9]+)</td><td>([0-9]*\.?[0-9]+)</td></tr>', b)
+
+            name_ls.append('total_socre')
+
+            cont_total = re.search(r'<div id="result-number">([0-9]*\.?[0-9]+)</div>', content, re.S).group(1)
+            score_ls.append(float(cont_total))
+
+            for i in cont_ls:
+                name_ls.append(i[0])
+                score_ls.append(float(i[1]))
+
         return name_ls, score_ls
 
 
 class Ares(Base):
     def __init__(self, writer):
-        super(Ares, self).__init__()
-        self.name = "ares"
-        self.folder = "ares"
-        self.writer = writer
-        self.df = DataFrame()
+        Base.__init__(self, "ares", "ares", writer)
 
     def run(self):
         src_ls = os.popen("ls %s" % self.folder).readlines()
@@ -132,7 +224,7 @@ class Ares(Base):
         score= re.search(r'<span id=3D"Geomean"><span class=3D"value">([\d\D]*?)</span>', cont).group(1)
         name_ls.append('Score')
         score_ls.append(float(score))
-        title_ls = ['air','basic','babylon','ML']
+        title_ls = ['air', 'basic','babylon','ML']
         for i in range(len(title_ls)):
             if i!=len(title_ls)-1:
                 sub_ls = re.findall(r'<div class=3D"{} test">[\d\D]*?<div class=3D"{} test">'.format(title_ls[i],title_ls[i+1]), cont)
@@ -151,15 +243,11 @@ class Ares(Base):
                 soc = re.search(r'(.*)<span class=3D"value">(.*?)</span>', m)
                 score_ls.append(float(soc.group(2)))
 
-        return name_ls,score_ls
+        return name_ls, score_ls
 
 class Webtooling(Base):
     def __init__(self, writer):
-        super(Webtooling, self).__init__()
-        self.name = "webtooling"
-        self.folder = "webtooling"
-        self.writer = writer
-        self.df = DataFrame()
+        Base.__init__(self, "webtooling", "webtooling", writer)
 
     def run(self):
         src_ls = os.popen("ls %s" % self.folder).readlines()
@@ -177,7 +265,7 @@ class Webtooling(Base):
 
         self.df.to_excel(self.writer, index=False, sheet_name=self.name)
 
-    def _run(self,file):
+    def _run(self, file):
         name_ls = []
         score_ls = []
         with open(file) as f:
